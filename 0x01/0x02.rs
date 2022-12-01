54adeb8e4c0c95c10b6036300def0f84dcdfe1e5
Mathias <mathias.andre@epitech.eu>
2022-12-01 16:01:32 +0100

    Day 0x01: First push

---
+++ 0x01/0x02.rs
@@ -0,0 +1,14 @@
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("input").expect("Unable to read input");
+    let mut elves: Vec<i32> = input
+        .split("\n\n")
+        .map(|items| items.split("\n").map(|item| item.parse::<i32>().unwrap()).sum())
+        .collect::<Vec<i32>>();
+    
+    elves.sort();
+    elves.reverse();
+    
+    println!("Best elves sum: {}", elves[..3].iter().sum::<i32>());
+}

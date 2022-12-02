209ed37219bad6893477696c89fa967547b64f5f
Mathias <mathias.andre@epitech.eu>
2022-12-02 12:04:43 +0100

    Day 0x02: First push

---
+++ 0x02/0x02.rs
@@ -0,0 +1,59 @@
+use std::fs;
+
+const WIN_SCORE: i32 = 6;
+const LOOSE_SCORE: i32 = 0;
+const TIE_SCORE: i32 = 3;
+
+fn main() {
+    let input = fs::read_to_string("input").expect("Unable to read input");
+    let rounds = input.split("\n").map(|round| round.split(" ").collect::<Vec<&str>>()).collect::<Vec<Vec<&str>>>();
+    let mut total_score = 0;
+
+    rounds.iter().for_each(|round| {
+        let loose_move: &str = match round[0] {
+            "A" => "Z",
+            "B" => "X",
+            "C" => "Y",
+            &_ => panic!("Unknown move")
+        };
+        let tie_move: &str = match round[0] {
+            "A" => "X",
+            "B" => "Y",
+            "C" => "Z",
+            &_ => panic!("Unknown move")
+        };
+        let win_move: &str = match round[0] {
+            "A" => "Y",
+            "B" => "Z",
+            "C" => "X",
+            &_ => panic!("Unknown move")
+        };
+
+        let move_to_do: &str = match round[1] {
+            "X" => loose_move,
+            "Y" => tie_move,
+            "Z" => win_move,
+            &_ => panic!("Unknown move")
+        };
+
+        total_score += {
+            if move_to_do == loose_move {
+                LOOSE_SCORE
+            } else if move_to_do == tie_move {
+                TIE_SCORE
+            } else if move_to_do == win_move {
+                WIN_SCORE
+            } else {
+                panic!("Unknown move")
+            }
+        };
+
+        total_score += match move_to_do {
+            "X" => 1,
+            "Y" => 2,
+            "Z" => 3,
+            &_ => panic!("Unknown move")
+        };
+    });
+    println!("Total score: {}", total_score);
+}

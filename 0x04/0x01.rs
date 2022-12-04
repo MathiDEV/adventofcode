5e10c8442aba404a9123a9517de1dd8335b06998
Mathias <mathias.andre@epitech.eu>
2022-12-04 17:35:25 +0100

    Day 0x04: First push

---
+++ 0x04/0x01.rs
@@ -0,0 +1,32 @@
+use std::fs;
+
+fn full_intersection(range1: (u32, u32), range2: (u32, u32)) -> bool {
+   range1.0 <= range2.0 && range1.1 >= range2.1  || range2.0 <= range1.0 && range2.1 >= range1.1
+}
+
+fn main() {
+    let input = fs::read_to_string("input").unwrap();
+    let intersects = input.split("\n")
+    .fold(0 as u32, |acc, pair| {
+        let elves = pair
+        .split(",")
+        .map(|elve| {
+            let elve_range = elve.split("-").collect::<Vec<&str>>();
+            (elve_range[0]
+                .parse::<u32>()
+                .expect("Unable to parse int"),
+            elve_range[1]
+                .parse::<u32>()
+                .expect("Unable to parse int")
+            )
+        })
+        .collect::<Vec<(u32, u32)>>();
+
+        if full_intersection(elves[0], elves[1]) {
+            acc + 1
+        } else {
+            acc
+        }
+    });
+    println!("Nb of full intetsections: {}", intersects);
+}
